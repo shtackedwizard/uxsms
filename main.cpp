015a82492ce4b697307541667009846228088027
shtackedwizard <90pistolpete@gmail.com>
2025-07-16 00:03:53 -0400

    gave it spunk

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <windows.h>
 #include <dwmapi.h>
 
+#pragma comment(lib, "dwmapi.lib")
+
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam) {
     DWMNCRENDERINGPOLICY pol = DWMNCRP_DISABLED;
-    DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &pol, sizeof(DWMNCRENDERINGPOLICY));
+    DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &pol, sizeof(pol));
     return TRUE;
 }
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     for (;;) {
-        Sleep(1);
+        Sleep(50);
         EnumDesktopWindows(NULL, EnumWindowsProc, NULL);
     }
     return 0;
